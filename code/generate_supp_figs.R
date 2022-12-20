45eb975af7e3ca903baad1c2ebeafb3c3f4e6c7f
Zachary Susswein <zsusswein@gmail.com>
2022-12-20 12:14:43 -0500

    Clean up organization and commenting

--- code/generate_supp_figs.R
+++ code/generate_supp_figs.R
@@ -1,5 +1,5 @@
-# This file compares the indoor outdoor metric, sigma, and county-level measurements of temperature and specific humidity
-
+# This file compares the indoor outdoor metric, sigma, and county-level measurements 
+#of temperature and specific humidity
 
 # Libraries ---------------------------------------------------------------
 
@@ -13,6 +13,7 @@ here::i_am('code/generate_supp_figs.R')
 
 con = dbConnect(duckdb::duckdb(), dbdir=":memory:")
 
+# Pull the county cluster assignments into memory
 dbExecute(con,
           "
           
@@ -23,59 +24,9 @@ dbExecute(con,
           FROM 'data/fips_modulclass.parquet'
           ")
 
- dbExecute(con,
-           "
-           
-          CREATE OR REPLACE TABLE trimmed_df AS 
-           
-          SELECT 
-            sigma.fips,
-            sigma.week,
-            clustid.cluster_id,
-            sigma.r_raw,
-            temp.temp,
-            SH.SH,
-            (r_raw - AVG(r_raw) OVER ()) / STDDEV_POP(r_raw) OVER () AS z_r_raw,
-            (temp - AVG(temp) OVER ()) / STDDEV_POP(temp) OVER () AS z_temp,
-            (SH - AVG(SH) OVER ()) / STDDEV_POP(SH) OVER () AS z_SH
-          FROM 
-            (
-            
-                       
-           SELECT
-            *,
-            (r_raw - AVG(r_raw) OVER ()) / STDDEV_POP(r_raw) OVER () AS z_r_raw_original
-           FROM 'data/indoor_outdoor_ratio_unsmoothed_WITHIN_CENTERD.parquet'
-           
-            ) sigma
-          INNER JOIN (
-          
-            SELECT 
-              fips,
-              date,
-              temp
-            FROM 'data/county_level_temp_kelvin_2018_2019.parquet'
-            
-            ) temp
-          ON sigma.week = temp.date 
-          AND sigma.fips = temp.fips
-          INNER JOIN (
-          
-            SELECT
-              fips,
-              date,
-              SH
-            FROM 
-              'data/county_level_specific_humidity_2018_2019.parquet'
-              
-          ) SH
-          ON sigma.week = SH.date
-          AND sigma.fips = SH.fips
-          LEFT JOIN clustid ON clustid.fips = sigma.fips
-          WHERE abs(z_r_raw_original) < 5 ;
-           
-           ")
-
+# Join temp, specific humidity, and indoor/outdoor metric ("sigma") together and
+# z-score each so they can be directly compared (i.e., all centered on 0 and deviations
+# are reported on the same scale.
 dbExecute(con,
           "
           CREATE OR REPLACE TABLE df AS
@@ -118,6 +69,65 @@ dbExecute(con,
 
           ")
 
+# Repeat the procedure from above, but pre-compute the z-scores ("z_raw_original")
+# trim off any that are above 5. This is meant as a robustness check to ensure that
+# any correlations or estimates of central tendency aren't driven by a few outliers
+# or some weird hard to spot tail behavior.
+dbExecute(con,
+           "
+           
+          CREATE OR REPLACE TABLE trimmed_df AS 
+           
+          -- Z score indoor/outdoor metric, humidity and temp
+          SELECT 
+            sigma.fips,
+            sigma.week,
+            clustid.cluster_id,
+            sigma.r_raw,
+            temp.temp,
+            SH.SH,
+            (r_raw - AVG(r_raw) OVER ()) / STDDEV_POP(r_raw) OVER () AS z_r_raw,
+            (temp - AVG(temp) OVER ()) / STDDEV_POP(temp) OVER () AS z_temp,
+            (SH - AVG(SH) OVER ()) / STDDEV_POP(SH) OVER () AS z_SH
+          FROM 
+            (
+            
+                       
+           SELECT
+            *,
+            (r_raw - AVG(r_raw) OVER ()) / STDDEV_POP(r_raw) OVER () AS z_r_raw_original
+           FROM 'data/indoor_outdoor_ratio_unsmoothed_WITHIN_CENTERED.parquet'
+           
+            ) sigma
+          INNER JOIN (
+          
+            SELECT 
+              fips,
+              date,
+              temp
+            FROM 'data/county_level_temp_kelvin_2018_2019.parquet'
+            
+            ) temp
+          ON sigma.week = temp.date 
+          AND sigma.fips = temp.fips
+          INNER JOIN (
+          
+            SELECT
+              fips,
+              date,
+              SH
+            FROM 
+              'data/county_level_specific_humidity_2018_2019.parquet'
+              
+          ) SH
+          ON sigma.week = SH.date
+          AND sigma.fips = SH.fips
+          LEFT JOIN clustid ON clustid.fips = sigma.fips
+          WHERE abs(z_r_raw_original) < 5 ;
+           
+           ")
+
+# Get the pearson correlation between all the combinations of the three metrics
 dbExecute(con,
           "
           
@@ -132,10 +142,9 @@ dbExecute(con,
           
           ")
 
+# Pull the processed results into memory
 df <- dbGetQuery(con, "SELECT * FROM df WHERE cluster_id IS NOT NULL;")
-
 summaries <- dbGetQuery(con, "SELECT * FROM summaries;")
-
 dbGetQuery(con, "SUMMARIZE summaries;") %>% 
   select(column_name, min, max, avg, std, q25, q50, q75) %>% 
   filter(column_name != "fips")
