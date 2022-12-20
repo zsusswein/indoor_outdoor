161ebcf539bce31e4560d4fef8eab4ee6fece653
Zachary Susswein <zsusswein@gmail.com>
2022-12-20 12:15:12 -0500

    Add working draft of fig to track prediction error

--- code/generate_supp_figs.R
+++ code/generate_supp_figs.R
@@ -156,30 +156,38 @@ p1 <- df %>%
   geom_smooth(aes(week, z_temp, group = cluster_id, color = cluster_id), linetype = 1, se = F)+
   theme_bw()+
   labs(x = element_blank(),
-       y = 'GAM fit to trimmed Z scores by cluster',
+       y = 'GAM fit to Z scores by cluster',
        color = 'Cluster ID')
 
 ggsave('figures/S1.png', p1)
   
 
+df <- dbGetQuery(con, 
+                 "
+SELECT
+    s.week,
+    c.clustid,
+    AVG(s.pred_error**2) AS rmse,
+    APPROX_QUANTILE(s.pred_error**2, 0.975) AS q975,
+    APPROX_QUANTILE(s.pred_error**2, 0.025) AS q025)
+FROM read_csv_auto('data/sine_curve_cluster_preds.parquet') s
+JOIN clustid c
+ON c.fips = s.fips
+GROUP BY c.clustid
 
-ggplot(df)+
-  geom_line(aes(z_SH, z_temp, group = fips, color = z_r_raw), alpha = .01)+
-  geom_abline()
+")
 
+p <- ggplot(df, aes(week, rmse, group = clustid, color = as.factor(clustid))+
+        geom_line()+
+        geom_ribbon(aes(x = week,
+                        ymin = q025,
+                        ymax = q975,
+                        fill = as.factor(clustid),
+                        group = clustid))+
+        theme_bw()+
+        labs(x = element_blank(),
+             y = 'Mean RMSE of sine curve fit',
+             color = 'Cluster',
+             fill = 'Cluster')
 
-
-ggplot(summaries)+
-  geom_point(aes(`corr(z_r_raw, z_temp)`, 
-                 `corr(z_r_raw, "z_SH")`, 
-                 color = `corr(z_temp, "z_SH")`))
-
-ggplot(summaries)+
-  geom_point(aes(`corr(z_r_raw, z_temp)`, 
-                 `corr(z_temp, "z_SH")`,
-                 color =  `corr(z_r_raw, z_temp)`))
-
-
-
-
-
+ggsave('figures/sine_curve_rmse.png', p)
